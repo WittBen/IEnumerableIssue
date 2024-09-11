fb21bf8d758013c1bf8f1d187f6efd15402af34c
WittBen <BenjaminWitt@web.de>
2024-09-11 18:22:17 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿var employees = GetEmployees();
+
+var count = employees.Count();
+
+
+foreach (var employee in employees)
+{
+  Console.WriteLine(employee);
+}
+
+
+
+IEnumerable<Employees> GetEmployees()
+{
+
+  var list = new List<Employees>();
+
+  var lines = File.ReadAllLines("./employees.csv");
+
+
+
+  //##############################################################
+  //Comment out this
+  //##############################################################
+  foreach (var line in lines)
+  {
+    var splitLine = line.Split(';');
+    yield return new Employees(splitLine[0], int.Parse(splitLine[1]));
+  }
+
+
+
+  ////##############################################################
+  ////Comment in the code below.
+  ////##############################################################
+
+  //foreach (var line in lines)
+  //{
+  //  var splitLine = line.Split(';');
+  //  list.Add(new Employees(splitLine[0], int.Parse(splitLine[1])));
+  //}
+  //return list;
+}
+
+
+record Employees(string Fullname, int Age);
